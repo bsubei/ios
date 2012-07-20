57c7c339402745bdec8e20335f05caeb7e8e1426
Bash <basheersubei@gmail.com>
2012-07-19 19:03:18 -0500

    created wjPrototype. Can switch views. That's all...

---
+++ wjPrototype/wjPrototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wjPrototype
+//
+//  Created by Basheer Subei on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ wjPrototype/wjPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  wjPrototype
+//
+//  Created by Basheer Subei on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
